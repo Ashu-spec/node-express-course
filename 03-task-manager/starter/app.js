94058c698d7a4d5e80cddecf5b448287fc3e4924
Ashu-spec <christianashish050@gmail.com>
2021-10-21 20:46:49 +0530

    Error handlers

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,29 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./DB/connect');
+const notFound = require('./middleware/Not_Found');
+require('dotenv').config();
+const errorHandlerMiddleware = require('./middleware/error_handler')
+
+// middleware
+app.use(express.static('./public'))
+app.use(express.json())
+
+// routes
+app.use('/api/v1/tasks', tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);            
+
+const port = process.env.PORT;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI).then(console.log("Connected To DB"))
+        app.listen(port, () => console.log(`app listening on port ${port}....`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
+
---
+++ 03-task-manager/starter/models/task.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: [true, "Must Provide a name"],
+        trim: true,
+        maxlength: [20, "Name cannot be longer then 20 characters"],
+    },
+    completed:{
+        type:Boolean,
+        default:false,
+    },
+});
+module.exports = mongoose.model('Task', TaskSchema);
